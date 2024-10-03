c8600a4508d45770849e7b6219b2e965cdabbc6f
Kami <jjolajitan2006@gmail.com>
2024-10-03 10:43:05 +0100

    Scraping works in background.js + data is transfered to google sheet

--- WebScrape/background.js
+++ WebScrape/background.js
@@ -1,23 +1,18 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.action === 'saveToGoogleSheets') {
-      const { scrapedText } = message;
-  
-      const backendURL = 'https://script.google.com/macros/s/AKfycbxe8dPU_AGKaW9jol8gDQT-nY-JG0DOClPIkiT6TU4oLpw5SsXHgiuenCukGQxX1sXj/exec'; 
-      console.log("backend url", backendURL);
-  
-      // Send the scraped text to Google Sheets via the backend URL
-      fetch(backendURL, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ text: scrapedText }),
-      })
-      .then(response => response.json())
-      .then(data => sendResponse({ success: true, data }))
-      .catch(error => sendResponse({ success: false, error }));
-  
-      // Indicate that we'll respond asynchronously
-      console.log("background.js: sending response");
-      return true;
-    }
-  });
-  
+  if (message.action === 'saveToGoogleSheets') {
+    const backendURL = 'https://script.google.com/macros/s/AKfycby30T1Cw79TDIoNCHM_ZZfmeXaC2y_iVDWvtUMtHXT0JGQLECITsIbVTQPf8Jelly4j/exec';
+
+    // Send the scraped text to Google Sheets via the backend URL
+    fetch(backendURL, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ text: message.scrapedText }),
+    })
+    .then(response => response.json())
+    .then(data => sendResponse({ success: true, data }))
+    .catch(error => sendResponse({ success: false, error: error.toString() }));
+
+    // Indicate that we'll respond asynchronously
+    return true;
+  }
+});
